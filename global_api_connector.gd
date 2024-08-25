96449161db75a47b0207b0a901fc512cca91b822
dmsrdnv@gmail.com <dmsrdnv@gmail.com>
2024-08-26 00:03:15 +0300

    add assistant_id, thread_id to API

--- global_api_connector.gd
+++ global_api_connector.gd
@@ -4,7 +4,10 @@ var url = "http://127.0.0.1:11231/messages/add"
 var headers = ["Content-Type: application/json"]
 
 func post_message(text, function):
-	var body = {"token": "test", "text": text}
+	var body = {"token": "test",
+				"text": text,
+				"assistant_id": "asst_XxTiih3YAwSOOtSAbIdlqW9b",
+				"thread_id": "thread_vLLHgxfreeZhhSwlb85qhg79"}
 	var request = HTTPRequest.new()
 	add_child(request)
 	request.connect("request_completed", function)
